2b83144ce66934374da43d37669798870aafc337
muammar johnson <muammarj@gmail.com>
2021-02-04 08:12:13 -0600

    finished

--- app/src/main/java/com/example/homeworkapplication/adapter/PersonAdapter.java
+++ app/src/main/java/com/example/homeworkapplication/adapter/PersonAdapter.java
@@ -1,5 +1,60 @@
 package com.example.homeworkapplication.adapter;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.homeworkapplication.R;
+import com.example.homeworkapplication.databinding.PersonItemBinding;
+import com.example.homeworkapplication.model.Person;
+
+import java.util.List;
+
 // TODO: 2/3/2021 Create adapter to take a List<Person>
-public class PersonAdapter {
+public class PersonAdapter  extends RecyclerView.Adapter<PersonAdapter.PersonViewHolder> {
+    private final List<Person> persons;
+    public PersonAdapter(List<Person> persons){
+        this.persons = persons;
+    }
+
+    @NonNull
+    @Override
+    public PersonViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.person_item
+//                ,parent
+//                ,false);
+        PersonItemBinding binding = PersonItemBinding.inflate(LayoutInflater.from(parent.getContext()),
+                parent,
+                false);
+        return new PersonViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PersonViewHolder holder, int position) {
+        Person person = persons.get(position);
+        holder.loadperson(person);
+    }
+
+    @Override
+    public int getItemCount() {
+        return persons.size();
+    }
+
+    static class PersonViewHolder extends RecyclerView.ViewHolder{
+        private final PersonItemBinding binding;
+        public PersonViewHolder(@NonNull PersonItemBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+        public void loadperson(Person person){
+
+            binding.age.setText(String.valueOf(person.getAge()));
+            binding.firstName.setText(person.getFirstName());
+            binding.lastName.setText(person.getLastName());
+            binding.personEmail.setText(person.getEmail());
+        }
+    }
 }
--- app/src/main/java/com/example/homeworkapplication/view/MainActivity.java
+++ app/src/main/java/com/example/homeworkapplication/view/MainActivity.java
@@ -1,16 +1,53 @@
 package com.example.homeworkapplication.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModel;
+
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.example.homeworkapplication.R;
+import com.example.homeworkapplication.adapter.PersonAdapter;
+import com.example.homeworkapplication.databinding.ActivityMainBinding;
+import com.example.homeworkapplication.model.Person;
+import com.example.homeworkapplication.viewmodel.MainViewModel;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
     // TODO: 2/3/2021 Get data from viewmodel and load into recyclerview
+
+    private ActivityMainBinding binding;
+    private MainViewModel viewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        binding.recycleview.setLayoutManager(linearLayoutManager);
+        viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        viewModel.loadPeopleList();
+        initObservers();
     }
+
+    private void initObservers(){
+
+        viewModel.getPeople().observe(this, new Observer<List<Person>>() {
+            @Override
+            public void onChanged(List<Person> people) {
+                PersonAdapter personAdapter = new PersonAdapter(people);
+                binding.recycleview.setAdapter(personAdapter);
+            }
+        });
+    }
+
 }
